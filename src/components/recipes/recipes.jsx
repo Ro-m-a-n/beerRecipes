5b7bbeaee78687832bc7ec3296166796e57805e0
roman liutyk <ggyrda18@gmail.com>
2023-07-19 14:31:20 +0300

    implement lazy loading

--- src/components/recipes/recipes.jsx
+++ src/components/recipes/recipes.jsx
@@ -1,11 +1,58 @@
+import { useEffect, useRef, useState } from "react";
 import { useStore } from "./../../store/store";
+
 export const Recipes = () => {
   const recipes = useStore((state) => state.recipes);
   const selected = useStore((state) => state.selected);
   const selectedUpdate = useStore((state) => state.selectedUpdate);
   const setCurrentRecipe = useStore((state) => state.setCurrentRecipe);
-  const first15recipes = recipes.slice(0, 15);
+  const changeInterval = useStore((state) => state.changeInterval);
+  const intervalFrom = useStore((state) => state.intervalFrom);
+  const intervalTo = useStore((state) => state.intervalTo);
+  const recipes15 = useStore((state) => state.recipes15);
+  const setRecipes15 = useStore((state) => state.setRecipes15);
   let selectedClass = "";
+
+  const [loadNext, setLoadNext] = useState(false);
+  const [loadPrev, setLoadPrev] = useState(false);
+
+  useEffect(() => {
+    if (recipes.length > 0) {
+      setRecipes15();
+    }
+  }, [recipes]); //on first load set 15 recipes to render
+  useEffect(() => {
+    if (loadNext && intervalTo < 25) {
+      setLoadNext(false);
+      changeInterval("down");
+      setRecipes15();
+    }
+  }, [loadNext, intervalTo]); //on reaching bottom of the screen
+  useEffect(() => {
+    if (loadPrev && intervalFrom > 0) {
+      setLoadPrev(false);
+      changeInterval("up");
+      setRecipes15();
+    }
+  }, [loadPrev, intervalFrom]); //on reaching top of the screen
+
+  useEffect(() => {
+    document.addEventListener("scroll", scrollHandler);
+    return () => document.removeEventListener("scroll", scrollHandler);
+  }, []);
+
+  const scrollHandler = (e) => {
+    if (
+      !loadNext &&
+      e.target.documentElement.scrollHeight -
+        (e.target.documentElement.scrollTop + window.innerHeight) <
+        10
+    ) {
+      setLoadNext(true); //on reaching bottom of the screen
+    } else if (!loadPrev && e.target.documentElement.scrollTop < 100) {
+      setLoadPrev(true); //on reaching top of the screen
+    }
+  };
 
   const handleClick = (event, index) => {
     event.preventDefault();
@@ -18,7 +65,7 @@ export const Recipes = () => {
     }
   };
 
-  const showRecipes = first15recipes.map((item) => {
+  const showRecipes = recipes15.map((item) => {
     if (selected.includes(item.id)) {
       selectedClass = "active";
     } else {
--- src/store/store.js
+++ src/store/store.js
@@ -6,7 +6,30 @@ export const useStore = create((set, get) => ({
   selected: [],
   currentRecipe: null,
   currentPage: 1,
-
+  intervalFrom: 0,
+  intervalTo: 15,
+  recipes15: [],
+  setRecipes15: () => {
+    let recipesSliced = get().recipes.slice(
+      get().intervalFrom,
+      get().intervalTo
+    );
+    set({
+      recipes15: recipesSliced,
+    });
+  },
+  changeInterval: (side) => {
+    let n = 0;
+    if (side === "up") {
+      n = -5;
+    } else if (side === "down") {
+      n = 5;
+    }
+    set((state) => ({
+      intervalFrom: state.intervalFrom + n,
+      intervalTo: state.intervalTo + n,
+    }));
+  },
   getRecipes: async () => {
     let res = await getRecipesAPI(get().currentPage);
     set((state) => ({ recipes: res, currentPage: state.currentPage + 1 }));
@@ -27,6 +50,4 @@ export const useStore = create((set, get) => ({
         selected: [],
       };
     }),
-
-  removeAllBears: () => set({ bears: 0 }),
 }));
